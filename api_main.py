7e978486a3716960b8f747b5488f32a7fa78a2d3
fersilverio <ssilveriofernando45@gmail.com>
2022-02-23 23:52:27 -0300

    ok

--- api_main.py
+++ api_main.py
@@ -12,13 +12,8 @@ CORS(app)
 def func():
     string_entrada = request.get_json()
     prob_final = get_prediction(string_entrada)
+    print(prob_final)
     return jsonify({'probabilidade': prob_final})
 
-@app.route('/teste')
-@cross_origin()
-def teste():
-    return jsonify({'resp' : 'teste'})
-
-
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(debug=True)
